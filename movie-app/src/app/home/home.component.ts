9eb6e1042756d21db58b8847a7b977e99a1386ea
LeonaKirijas <leonakirijas@gmail.com>
2024-01-07 02:01:27 +0100

    Api connected and data is show

--- movie-app/src/app/home/home.component.ts
+++ movie-app/src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data: any;
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.fetchData().pipe(
+      catchError(error => {
+        console.error('Error fetching data: ', error);
+        return of([]); // or handle the error appropriately
+      })
+    ).subscribe(
+      (responseData) => {
+        this.data = responseData;
+      }
+    );
+  }
 }
